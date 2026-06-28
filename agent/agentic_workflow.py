71bfd4b3d5d303cef9d0f9c773dd22f895ca254f
NikhilGoral28 <nikhilgoral587@gmail.com>
2026-06-28 08:51:36 +0530

    agent builder

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -3,10 +3,10 @@ from prompt_library.prompt import SYSTEM_PROMPT
 
 from tools.weather_info_tool import WeatherInfoTool
 from tools.place_search_tool import PlaceSearchTool
-from tools.expence_calculator_tool import CalculatorTool
+from tools.expense_calculator_tool import CalculatorTool
 from tools.currency_conversion_tool import CurrencyConverterTool
 
-from langgraph.graph import StateGraph, MessageGraph, END, START
+from langgraph.graph import StateGraph, MessageGraph, END, START,MessagesState
 from langgraph.prebuilt import ToolNode,tools_condition
 
 
@@ -15,13 +15,32 @@ from langgraph.prebuilt import ToolNode,tools_condition
 class GraphBuilder():
 
     def __init__(self):
-        pass
+        self.tools = [
+            #WeatherInfoTool(),
+            #PlaceSearchTool(),
+            #CalculatorTool(),
+            #CurrencyConverterTool()
+        ]
 
-    def agent_function(self):
-        pass
+    def agent_function(self, state:MessagesState):
+        """main agent function"""
+        user_question = state['messages']
+        input_question = [self.system_prompt] + user_question
+        response = self.llm_with_tools.invoke(input_question)
+        return {"messages": [response]}
+    
 
     def build_graph(self):
-        pass
+        graph_builder = StateGraph(MessageGraph)
+        graph_builder.add_node('agent',self.agent_function)
+        graph_builder.add_node('tools',ToolNode(tools=self.tools))
+        graph_builder.add_edge(START,'agent')
+        graph_builder.add_conditional_edges('agent',tools_condition)
+        graph_builder.add_edge('tools','agent')
+        graph_builder.add_edge('agent',END)
+
+        self.graph = graph_builder.compile()
+        return self.graph
 
     def __call__(self):
-        pass
+        return self.build_graph()
